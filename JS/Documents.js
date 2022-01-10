060283dd1a7f3aa5a7c5b655c6020f0899370296
isingle <35318953+isinglever@users.noreply.github.com>
2022-01-10 12:23:04 +0800

    Update Documents.js

--- JS/Documents.js
+++ JS/Documents.js
@@ -1,12 +1,36 @@
 var obj= {
-  "originalTransactionId" : "20000625420102",
-  "subscriptionState" : "trial",
-  "isInGracePeriod" : false,
-  "subscriptionExpirationDate" : "17:48 25/11/2099",
-  "isDocuments6User" : true,
-  "isEligibleForIntroPeriod" : false,
-  "subscriptionAutoRenewStatus" : "autoRenewOff",
-  "subscriptionReceiptId" : "1530908572000"
+  "originalTransactionId": 500000982615091,
+  "inAppStates": [
+    {
+      "originalTransactionId": 500000982615091,
+      "productId": "com.readdle.ReaddleDocsIPad.subscription.month10_allusers",
+      "subscriptionState": "trial",
+      "productName": "subscription",
+      "subscriptionExpirationDate": "03:26 17\/01\/2029",
+      "isEligibleForIntroPeriod": false,
+      "subscriptionAutoRenewStatus": "autoRenewOn",
+      "type": "subscription",
+      "isInGracePeriod": false,
+      "isInBillingRetryPeriod": false,
+      "entitlements": [
+      ]
+    },
+    {
+      "originalTransactionId": "0000",
+      "entitlements": [
+      ],
+      "type": "custom purchase",
+      "productId": "documents6-user"
+    }
+  ],
+  "chargingPlatform": "iOS AppStore",
+  "receiptStatus": "ok",
+  "bundleId": "com.readdle.ReaddleDocsIPad",
+  "receiptId": 1490008805000,
+  "statisticsInfo": {
+    "events": [
+    ]
+  }
 };
 
 $done({body: JSON.stringify(obj)});
