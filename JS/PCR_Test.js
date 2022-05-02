94297c6b6a0695ef064b1c8c628b3a4ff4c022a4
isingle <35318953+isinglever@users.noreply.github.com>
2022-05-02 19:19:26 +0800

    Create PCR_Test.js

---
+++ JS/PCR_Test.js
@@ -0,0 +1,19 @@
+var obj = {
+  "errcode" : "0",
+  "data" : [
+    {
+      "hscysj" : "2022-05-01 09:02:30.0",
+      "hsjcsj" : "2022-05-01 21:21:07.0",
+      "hsjcjq" : "阴性",
+      "hsjcjgmc" : "成都禾力升医学检验实验室",
+      "inDay7" : true,
+      "xm" : "王平",
+      "zjhm" : "51**************11",
+      "hsjcjg" : "阴性"
+    }
+  ],
+  "requestId" : null,
+  "errmsg" : "SUCCESS"
+};
+
+$done({body: JSON.stringify(obj)});
